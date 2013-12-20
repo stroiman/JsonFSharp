0a53c57e784c421cb2813370eedd264b5d3f07dd
Peter Strøiman <peter@stroiman.com>
2013-12-20 21:12:51 +0100

    Created skeleton specs project

---
+++ src/JsonFSharp.Specs/Program.fs
@@ -0,0 +1,13 @@
+﻿open FSpec.Core.DslV2
+
+let specs =
+    describe "Parser" [
+        it "works" pending
+    ]
+
+let summary =
+    [ specs ]
+    |> List.map run
+    |> List.map getSummary
+    |> List.sum
+printfn "Summary: %A" summary
