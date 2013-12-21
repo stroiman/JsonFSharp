f2853a42c4d4799ae800fc6b2f5e2fa8fb393db4
Peter Strøiman <peter@stroiman.com>
2013-12-21 21:53:35 +0100

    Implemented scientific numbers

--- src/JsonFSharp.Specs/PrimitiveTypesSpec.fs
+++ src/JsonFSharp.Specs/PrimitiveTypesSpec.fs
@@ -55,6 +55,8 @@ let specs =
             numberSpec "parse whole number" "123" 123.0
             numberSpec "parse whole number" "123.25" 123.25
             numberSpec "parse negative number" "-123.25" -123.25
+            numberSpec "parse scientific number" "1.2e2" 120.0
+            numberSpec "parse scientific number with uppercase E" "1.2E2" 120.0
         ]
     ]
 
