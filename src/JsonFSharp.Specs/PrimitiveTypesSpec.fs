f9fa9570c1ea9607a865e14add5c86462de3746d
Peter Strøiman <peter@stroiman.com>
2013-12-21 18:58:51 +0100

    Escape characters \" \\ \/ \b \f \n \r \t

--- src/JsonFSharp.Specs/PrimitiveTypesSpec.fs
+++ src/JsonFSharp.Specs/PrimitiveTypesSpec.fs
@@ -18,10 +18,45 @@ let specs =
                 |> shouldEqualString "dummy"
 
             describe "escape characters" [
+                it """parses \" """ <| fun () ->
+                    "\"\\\"\"" 
+                    |> JsonParser.parse
+                    |> shouldEqualString "\""
+
+                it "parses \\\\" <| fun () ->
+                    "\"\\\\\"" 
+                    |> JsonParser.parse
+                    |> shouldEqualString "\\"
+
+                it "parses \\/" <| fun () ->
+                    "\"\\/\"" 
+                    |> JsonParser.parse
+                    |> shouldEqualString "/"
+
                 it "parses \\b" <| fun () ->
                     "\"\\b\"" 
                     |> JsonParser.parse
                     |> shouldEqualString "\b"
+
+                it "parses \\f" <| fun () ->
+                    "\"\\f\"" 
+                    |> JsonParser.parse
+                    |> shouldEqualString "\f"
+
+                it "parses \\n" <| fun () ->
+                    "\"\\n\"" 
+                    |> JsonParser.parse
+                    |> shouldEqualString "\n"
+
+                it "parses \\r" <| fun () ->
+                    "\"\\r\"" 
+                    |> JsonParser.parse
+                    |> shouldEqualString "\r"
+
+                it "parses \\t" <| fun () ->
+                    "\"\\t\"" 
+                    |> JsonParser.parse
+                    |> shouldEqualString "\t"
 
                 it "parses string containg \\b" <| fun () ->
                     "\"dummy\\bdummy\"" 
