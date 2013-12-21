d608d7493e7d19aa91d2b6ea211608156c7bc70a
Peter Strøiman <peter@stroiman.com>
2013-12-21 22:04:56 +0100

    Extracted test helpers

---
+++ src/JsonFSharp.Specs/Helpers.fs
@@ -0,0 +1,22 @@
+﻿module Helpers
+open FSpec.Core.DslV2
+open FSpec.Core.MatchersV2
+open JsonFSharp
+
+let shouldEqualJson expected actual =
+    actual |> should equal (Success expected)
+
+let shouldEqualString expected actual =
+    actual |> shouldEqualJson (JsonString expected)
+
+let spec name input expected =
+    it name <| fun () ->
+        input
+        |> JsonParser.parse
+        |> shouldEqualJson expected
+        
+let stringSpec name input expectedOutput =
+    spec name input (JsonString expectedOutput)
+
+let numberSpec name input expectedOutput =
+    spec name input (JsonNumber expectedOutput)
