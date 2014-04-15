2628987cb1a817db41fa61dc1f16351fc3f329c6
Peter Strøiman <peter@stroiman.com>
2014-04-15 12:29:47 +0200

    Refactored to use two-track paradigm

--- src/JsonFSharp/JsonParser.fs
+++ src/JsonFSharp/JsonParser.fs
@@ -3,6 +3,7 @@ open System.IO
 open JsonFSharp.Lexer
 open JsonFSharp.Parsers
 open System.Linq
+open TwoTrack
 
 type JsonInput =
     | StringInput of string
@@ -25,41 +26,33 @@ let parse input =
 
 
 let toInstance<'T> json =
-    let getObjectData json =
-        match json with
-        | JsonString(x) -> x :> System.Object
-        | JsonNumber(x) -> x :> System.Object
-        | _ -> failwith "Not implemented"
-
     let rec toInstanceOfType (targetType: System.Type) json =
-
         match json with
         | JsonObject(obj) ->
-            let getValue = obj.TryFind
+            let getValue name = 
+                name 
+                |> obj.TryFind
+                |> Result.FromOption (sprintf "could not find data for record value '%s'" name)
+            
             let getConstructorArgument (arg: System.Reflection.ParameterInfo) =
-                let jsonValueOption = getValue arg.Name
-                match jsonValueOption with
-                | None -> Failure(sprintf "could not find data for record value '%s'" arg.Name)
-                | Some(jsonValue) ->
+                let toObj value = Success(System.Convert.ChangeType(value :> System.Object, arg.ParameterType))
+                let convertToType jsonValue =
                     match jsonValue with
-                    | JsonObject(x) ->
-                        match toInstanceOfType arg.ParameterType jsonValue with
-                        | Success(x) -> Success(x)
-                        | Failure(x) -> Failure(x)
-                    | _ ->
-                        let value = jsonValue |> getObjectData
-                        Success(System.Convert.ChangeType(value, arg.ParameterType))
+                    | JsonObject(x) -> toInstanceOfType arg.ParameterType jsonValue 
+                    | JsonString(x) -> toObj x
+                    | JsonNumber(x) -> toObj x
+                    | _ -> failwith "Not implemented"
+                getValue arg.Name
+                >>= convertToType
 
             let rec getConstructorParameters (args: System.Reflection.ParameterInfo list) =
                 match args with
                 | [] -> Success([])
                 | head::tail ->
-                    match getConstructorParameters tail with
-                    | Failure(x) -> Failure(x)
-                    | Success(x) -> 
-                        match getConstructorArgument head with
-                        | Success(y) -> Success(y :: x)
-                        | Failure(y) -> Failure(y)
+                    getConstructorArgument head 
+                    |> bind (fun head -> 
+                        getConstructorParameters tail 
+                        |> bind (fun tail -> Success(head::tail)))
 
             let ctor = targetType.GetConstructors().Single()
             let ctorParameters = 
--- src/JsonFSharp/Types.fs
+++ src/JsonFSharp/Types.fs
@@ -8,8 +8,22 @@ type JsonValue =
     | JsonArray of JsonValue list
     | JsonObject of Map<string,JsonValue>
 
-type Result<'TSuccess> =
+type Result<'TSuccess, 'TFailure> =
     | Success of 'TSuccess
-    | Failure of string
+    | Failure of 'TFailure
+    with 
+        static member FromOption (errorMsg : 'TFailure) (option : 'TSuccess option) =
+            match option with
+            | Some s -> Success s
+            | None -> Failure errorMsg
 
-type ParseResult = Result<JsonValue>
+type ParseResult = Result<JsonValue,string>
+
+module TwoTrack =
+    let bind switchFunction twoTrackInput = 
+        match twoTrackInput with
+        | Success s -> switchFunction s
+        | Failure f -> Failure f
+
+    let (>>=) twoTrackInput switchFunction = 
+        bind switchFunction twoTrackInput 
